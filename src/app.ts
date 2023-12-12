fbb6a648d6a0eb451e389b98b5f2f157a6cfbc3a
unknown <jaberhasan2002@gmail.com>
2023-12-12 15:23:48 +0600

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express, { Application, Request, Response } from 'express';
+
+import cors from 'cors';
 const app: Application = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('hello world!');
+  res.send('hello world! assignment-3');
+});
+app.post('/', (req, res) => {
+  res.send('mongodb is working FIne');
 });
 
 export default app;
